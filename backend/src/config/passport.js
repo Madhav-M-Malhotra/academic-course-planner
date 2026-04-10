bd8603cf1104d39673caf5b368d7eae0e6a062d7
Madhav-M-Malhotra <madhavmalhotra107@gmail.com>
2026-04-10 23:49:51 +0530

    used prisma schema to add data from db for serialization and deserialization

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,5 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 //Passport Google OAuth2 Strategy
 passport.use(new GoogleStrategy({
@@ -7,9 +9,17 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
-  function(accessToken, refreshToken, profile, cb) {
+  async(accessToken, refreshToken, profile, cb) => {
     if(profile._json.hd === process.env.UNI_DOMAIN){
-      return cb(null, profile._json);
+      const user = await prisma.student.findUnique({
+        where: { email: profile._json.email },
+        select: {
+          id: true,
+          major: true,
+          minor: true
+        }
+      });
+      return cb(null, user);
     }else{
       return cb(null, false);
     }
