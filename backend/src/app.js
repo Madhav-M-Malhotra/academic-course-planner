3ee2b3385842ce995e87e3248a661a5d95a6cd55
Madhav-M-Malhotra <madhavmalhotra107@gmail.com>
2026-04-10 23:50:56 +0530

    fixed bugs and added routers

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,14 @@ const session = require("express-session");
 const path = require("path");
 const passport = require("passport");
 require("./config/passport.js");
+//Middleware imports
+const { isLoggedIn } = require("./middleware");
 //Router imports
 const authRouter = require("./routes/auth.routes.js");
-const acpRouter = require("./routes/acp.routes.js");
+const mainRouter = require("./routes/main.routes.js");
+const courseRouter = require("./routes/course.routes.js");
+const planRouter = require("./routes/plan.routes.js");
+const timetableRouter = require("./routes/timetable.routes.js");
 
 app.use(express.json());//for reading json requests
 app.use(express.urlencoded({extended: true}));//to parse HTML Forms data
@@ -16,12 +21,12 @@ app.use(express.static(path.join(__dirname,"../../frontend")));//static files (f
 //Express Session
 const sessionOptions = {
     secret: process.env.SESSION_SECRET,
-    resave: false,
+    resave: false, //session resaved only if modified during the request
     saveUninitialized: true,
     cookie: {
-        expires: Date.now() + 24*60*60*1000,
-        maxAge: 24*60*60*1000,
-        httpOnly: true
+        expires: Date.now() + 24*60*60*1000, //expirese in 24hrs
+        maxAge: 24*60*60*1000, //maxage 24hrs
+        httpOnly: true //to protect against XSS attacks
     }
 };
 app.use(session(sessionOptions));
@@ -37,5 +42,18 @@ app.listen(process.env.PORT,()=>{
 
 //Login Routes
 app.use('/auth', authRouter);
-//ACP Routes
-app.use('/', acpRouter);
+
+//authentication before allowing access to anything beyond login routes
+app.use(isLoggedIn);
+
+//Course Routes
+app.use('/courses', courseRouter);
+
+//Plan Routes
+app.use('/plan', planRouter);
+
+//Timetable Routes
+app.use('/timetable', timetableRouter);
+
+//Main page Routes
+app.get('/', mainRouter);
